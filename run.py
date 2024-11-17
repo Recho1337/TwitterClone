01f5474c0834681a4013a0078bbd26b175afbbe2
Recho1337 <roberthartles1235@outlook.com>
2024-11-17 16:51:38 +0000

    waitress over flask server

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import logging
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('waitress')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    logger.info("Starting server with Waitress...")
+    serve(app, host='0.0.0.0', port=5000)
